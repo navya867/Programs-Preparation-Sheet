41531377c262dcb2f29afdc9662bddafb49d7a9e
Navya Sree <89930278+navya867@users.noreply.github.com>
2026-09-04 23:56:34 +0530

    Update Merge_sort.py

--- DSA/Sorting/Merge_sort.py
+++ DSA/Sorting/Merge_sort.py
@@ -1,1 +1,23 @@
-def mergesort ()
+class Solution: 
+    def Merge(self, arr,left,mid,right):
+        
+        temp=[]
+        low=left
+        high=mid+1
+
+
+
+    def mergesort(self,arr,low,high):
+      if low>=high:
+        return
+      mid=low+high // 2
+      self.merge(self,arr,low,mid
+
+
+sol=Solution()
+arr=[10,34,5,6,2,56]
+n=len(arr)
+i=0
+arr_result=sol.InsertionSort(arr,n,i)
+print(arr_result)
+
